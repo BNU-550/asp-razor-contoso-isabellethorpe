543b410bad76a81c28ed8a953bb16f60eb6e3108
Isabelle Thorpe <isabelle_thorpe@outlook.com>
2021-10-27 15:52:11 +0100

    Model Classes added

---
+++ Models/Enrollment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP_Razor_Contoso.Models
+{
+    public enum Grades
+        
+    { 
+        A, B, C, D, F
+    }
+        public class Enrollment
+        {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+            [DisplayFormat(NullDisplayText = "No grade")]
+        public Nullable<Grades> Grade { get; set; }
+
+        // Navigation properties
+        public virtual Course Course { get; set; }         
+        public virtual Student Student { get; set; }
+        }
+}
